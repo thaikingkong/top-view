97b1f7704530bc655d6570c47deac7a41b7a377e
ThaiDuiNe <92641592+thaikingkong@users.noreply.github.com>
2023-05-12 13:43:34 +0700

    Update top_view.cpp

--- top_view.cpp
+++ top_view.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
 
@@ -45,3 +45,48 @@ class Node {
             right = NULL;
         }
 };
+
+*/
+
+    void topView(Node * root) {
+        queue<pair<int,Node*>> q;
+        q.push(make_pair(0,root));
+        map<int,Node*> ans;
+        while(!q.empty()){
+            auto i=q.front();
+            if(i.second != nullptr){
+                ans.insert(i);
+                q.push(make_pair(i.first+1,i.second->right));
+                q.push(make_pair(i.first-1,i.second->left));
+            }
+            q.pop();
+            i=q.front();
+        }
+        vector<int> A;
+        for (auto i:ans)
+            A.push_back(i.second->data);
+        sort(A.begin(), A.end()); 
+        for (int j=0;j<A.size();j++)
+        	cout << A[j] << " ";
+    }
+
+}; //End of Solution
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+	myTree.topView(root);
+    return 0;
+}
